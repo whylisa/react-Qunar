b8781188f540a831d92cc38add50b30d144f8fa8
haoyue.wang <843655240@qq.com>
2020-05-19 16:09:44 +0800

    Context静态数据传递

--- get-react/src/App.js
+++ get-react/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {createContext} from 'react';
 import './App.css';
-
-function App() {
+const BatterContext = createContext()
+function Leaf() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BatterContext.Consumer>
+        {
+          battery => <h1> Battery: {battery}</h1>
+        }
+    </BatterContext.Consumer>
+  )
+}
+function Middle() {
+  return (
+    <Leaf />
+  )
+}
+function App() {
+  
+  return (
+    <BatterContext.Provider value={60}>
+        <Middle></Middle>
+      </BatterContext.Provider>
   );
 }
 
--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
 ## 初始化
   - npx create-react-app 项目名字
   - react-devTools 谷歌扩展程序 
+## 基础概念
+  - context(上下文)
+    - 提供了一种方式，能够让数据在组件树种传递，而不必一级一级手动传递
+    - createContext()
+  - ContextType
+  - lazy
+  - Suspense
+  - memo
     
